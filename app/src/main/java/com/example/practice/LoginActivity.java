914ea5f5602988e01e3a837fb615c118be562932
Jiseok <sapiens2525@naver.com>
2023-11-25 02:52:57 +0900

    0252

--- app/src/main/java/com/example/practice/LoginActivity.java
+++ app/src/main/java/com/example/practice/LoginActivity.java
@@ -2,6 +2,7 @@ package com.example.practice;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -16,7 +17,6 @@ public class LoginActivity extends AppCompatActivity {
 
 
         Button bt = findViewById(R.id.button2);
-
         bt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
--- app/src/main/java/com/example/practice/MainActivity.java
+++ app/src/main/java/com/example/practice/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.practice;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +12,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        Button hamburger = findViewById(R.id.hamburger);
+        hamburger.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), ChatsHamburgerActivity.class);
+                startActivity(i);
+            }
+        });
+
 
         Intent intent = new Intent(this, LoginActivity.class);
         startActivity(intent);
