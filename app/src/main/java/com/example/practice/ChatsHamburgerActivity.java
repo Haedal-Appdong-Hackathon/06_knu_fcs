88abb68f20ad1f3060d20ab149954868edbadb39
Jiseok <sapiens2525@naver.com>
2023-11-25 04:00:14 +0900

    ..

--- app/src/main/java/com/example/practice/ChatsHamburgerActivity.java
+++ app/src/main/java/com/example/practice/ChatsHamburgerActivity.java
@@ -2,11 +2,13 @@ package com.example.practice;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class ChatsHamburgerActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d("test", "hamburger on");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.chats_hamburger);
     }
