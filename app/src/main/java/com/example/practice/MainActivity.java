27c0d8e1d76091aad57502131bcd0a6bd6e5febf
Jiseok <sapiens2525@naver.com>
2023-11-25 03:13:58 +0900

    ..

--- app/src/main/java/com/example/practice/MainActivity.java
+++ app/src/main/java/com/example/practice/MainActivity.java
@@ -22,8 +22,9 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-
+        /*
         Intent intent = new Intent(this, LoginActivity.class);
         startActivity(intent);
+        */
     }
 }
