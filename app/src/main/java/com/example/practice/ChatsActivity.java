3f51a832dcc36bd43682d3cd20c33d1a597549ac
swisdom784 <jseo0902@knu.ac.kr>
2023-11-25 02:03:37 +0900

    add chats_western.xml

--- app/src/main/java/com/example/practice/ChatsActivity.java
+++ app/src/main/java/com/example/practice/ChatsActivity.java
@@ -8,6 +8,6 @@ public class ChatsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.chats);
+        setContentView(R.layout.chats_hamburger);
     }
 }
