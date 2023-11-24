5de76a2a499c776ad748249f12827959ddf44363
swisdom784 <jseo0902@knu.ac.kr>
2023-11-24 23:43:40 +0900

    gitignore 재적용

---
+++ app/src/main/java/com/example/practice/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.practice;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.page3_1);
+    }
+}
+//ghgh
+//ttgdgh
---
+++ app/src/main/java/com/example/practice/MainActivity6.java
@@ -0,0 +1,16 @@
+package com.example.practice;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity6 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.page1);
+    }
+}
+//ghgh
+//ttgdgh
