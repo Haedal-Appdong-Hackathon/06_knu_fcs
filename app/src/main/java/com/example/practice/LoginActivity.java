f95e256af34d180caaad1b41e2a660082659e176
Jiseok <sapiens2525@naver.com>
2023-11-25 02:27:30 +0900

    0227cjs

--- app/src/main/java/com/example/practice/LoginActivity.java
+++ app/src/main/java/com/example/practice/LoginActivity.java
@@ -3,6 +3,9 @@ package com.example.practice;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +13,25 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
+
+
+        Button bt = findViewById(R.id.button2);
+
+        bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showToast("로그인 성공!");
+                finish();
+            }
+        });
     }
+
+    private void showToast (String message){
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+    }
+
+
+    //정보입력
+    //confirm
+    //go!
 }
