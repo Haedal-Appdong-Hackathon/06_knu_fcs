875d9bf1abbf6803fa2d9b866583afda98b2e905
Jiseok <sapiens2525@naver.com>
2023-11-25 05:00:48 +0900

    ..

--- app/src/main/java/com/example/practice/ChatsHamburgerActivity.java
+++ app/src/main/java/com/example/practice/ChatsHamburgerActivity.java
@@ -1,8 +1,13 @@
 package com.example.practice;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class ChatsHamburgerActivity extends AppCompatActivity {
 
@@ -11,5 +16,18 @@ public class ChatsHamburgerActivity extends AppCompatActivity {
         Log.d("test", "hamburger on");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.chats_hamburger);
+
+        TextView chat = findViewById(R.id.textView9);
+        chat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    Intent i = new Intent(ChatsHamburgerActivity.this, ChatroomActivity.class);
+                    startActivity(i);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 }
--- app/src/main/java/com/example/practice/ChatsWesternActivity.java
+++ app/src/main/java/com/example/practice/ChatsWesternActivity.java
@@ -1,7 +1,11 @@
 package com.example.practice;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class ChatsWesternActivity extends AppCompatActivity {
 
@@ -9,5 +13,18 @@ public class ChatsWesternActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.chats_western);
+
+        TextView chat = findViewById(R.id.textView9);
+        chat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    Intent i = new Intent(ChatsWesternActivity.this, ChatroomActivity.class);
+                    startActivity(i);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 }
